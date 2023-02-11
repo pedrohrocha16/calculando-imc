90aae96c0fb3ca938f0133914c0d4f2dcccef60d
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-02-11 09:29:02 -0300

    Corrigindo cores; lógica

--- assets/main.js
+++ assets/main.js
@@ -12,11 +12,19 @@ function calcular(){
     var imc = res1 / (res2 * res2)
     var imcFinal = `${imc.toFixed(6)}` * 10000
 
-    if (res1 > 300 || res2 > 230){
-        alert('Valores Inválidos. Tente novamente!')
-    } else {
-        resultado.innerHTML = `Seu IMC é: ${imcFinal}.<br>
+    if (res1 >= 300 || res2 >= 230){
+        itemRes.innerHTML = 'Valores Inválidos.<br>Tente novamente!'
+        itemRes.style.color = 'white'
+        itemRes.style.background = 'red'
+    } else if (res1 <= 0 || res2 < 50){
+        itemRes.innerHTML = 'Valores Inválidos.<br>Tente novamente!'
+        itemRes.style.color = 'white'
+        itemRes.style.background = 'red'
+    }
+    
+    else {
+        resultado.innerHTML = `Seu IMC é: ${imcFinal}<br>
         Confira na tabela abaixo a sua classificação.`
-        itemRes.style.background ="#75ff0c"
+        itemRes.style.background ="rgb(9 2 110)"
     }   
 } 
