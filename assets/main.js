c4ccbb2966fe65695cd3531fd927cda76492be9c
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-02-18 17:05:22 -0300

    Corrigindo lógica

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,4 @@
 var sendDados = document.getElementById('btn-enviar')
-
 sendDados.addEventListener('click', calcular)
 
 function calcular(){
@@ -14,17 +13,14 @@ function calcular(){
     var imc = res1 / (res2 * res2)
     var imcFinal = `${imc.toFixed(5)}` * 10000
 
-    if (res1 <= 45 || res2 <= 145){
-        resultado.innerHTML = `Valores inválidos, corrija-os e tente novamente.<br>
-         Valores mínimos para cálculo:<br>
-         Peso em kg: 45*<br>
-         Altura em cm: 145*`
+    //check campos vazios, valores inválidos
+    if (btnNome.value.length == 0 || btnkg.value.length == 0|| btnAlt.value.length == 0){
+       resultado.innerHTML ='Impossível calcular, verifique se os campos estão preenchidos corretamente.'
     }
-    if (res1 >= 200 || res2 >= 240){
-        resultado.innerHTML = `Valores excedidos! Corrija-os e tente novamente.`
-    } else {
-        resultado.innerHTML = `&#9989; Olá ${res0}! Seu IMC é: ${imcFinal}<br>
-        Confira na tabela abaixo a sua classificação. &#11015;`
-        itemRes.style.background ="rgb(9 2 110)"
-    }   
+    if (res1 <= 30 || res2 < 130){
+        alert('Valores inválidos! Por favor, verifique os dados.')
+    }
+    else {
+        alert('ok')
+    }
 } 
