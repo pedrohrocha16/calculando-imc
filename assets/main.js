a36193a3a4462a1c8015afc07fc750d06661a83a
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-02-18 16:15:36 -0300

    Corrigindo lógica

--- assets/main.js
+++ assets/main.js
@@ -12,13 +12,17 @@ function calcular(){
     var res1 = Number(btnkg.value)
     var res2 = Number(btnAlt.value)
     var imc = res1 / (res2 * res2)
-    var imcFinal = `${imc.toFixed(6)}` * 10000
+    var imcFinal = `${imc.toFixed(5)}` * 10000
 
-    if (res1 >= 199 || res2 >= 230){
-      alert('Valores Inválidos. Tente novamente!')
-    } else if (res1 <= 0 || res2 < 50){
-        alert('Valores Inválidos. Tente novamente!')
-    }else {
+    if (res1 <= 45 || res2 <= 145){
+        resultado.innerHTML = `Valores inválidos, corrija-os e tente novamente.<br>
+         Valores mínimos para cálculo:<br>
+         Peso em kg: 45*<br>
+         Altura em cm: 145*`
+    }
+    if (res1 >= 200 || res2 >= 240){
+        resultado.innerHTML = `Valores excedidos! Corrija-os e tente novamente.`
+    } else {
         resultado.innerHTML = `&#9989; Olá ${res0}! Seu IMC é: ${imcFinal}<br>
         Confira na tabela abaixo a sua classificação. &#11015;`
         itemRes.style.background ="rgb(9 2 110)"
