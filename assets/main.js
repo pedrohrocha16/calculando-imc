caf18b455c6ba06d34bc0a1bbbc7ee0076bc33dc
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-03-05 07:53:24 -0300

    Corrigindo Bug lógica e alteração de lógica

--- assets/main.js
+++ assets/main.js
@@ -3,23 +3,21 @@ var sendDados = document.getElementById('btn-enviar')
 sendDados.addEventListener('click', calcular)
 
 function calcular(){
-    var btnNome = document.getElementById ('nome')
     var btnkg = document.getElementById ('kg')
     var btnAlt = document.getElementById ('altura')
     var itemRes = document.getElementsByClassName('resultado')[0]
     var resultado = document.getElementById('msg-res')
-    var res0 = String(btnNome.value)
     var res1 = Number(btnkg.value)
     var res2 = Number(btnAlt.value)
     var imc = res1 / (res2 * res2)
-    var imcFinal = `${imc.toFixed(6)}` * 10000
+    var imcFinal = `${imc.toFixed(5)}` * 10000
 
     if (res1 >= 199 || res2 >= 230){
       alert('Valores Inválidos. Tente novamente!')
     } else if (res1 <= 0 || res2 < 50){
         alert('Valores Inválidos. Tente novamente!')
     }else {
-        resultado.innerHTML = `&#9989; Olá ${res0}! Seu IMC é: ${imcFinal}<br>
+        resultado.innerHTML = `&#9989; Olá! Seu IMC é: ${imcFinal}<br>
         Confira na tabela abaixo a sua classificação. &#11015;`
         itemRes.style.background ="rgb(9 2 110)"
     }   
